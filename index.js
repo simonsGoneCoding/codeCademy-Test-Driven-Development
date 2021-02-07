0ef950d3ccb726f361e9e56a979c68fcc9293cba
Szymon Warszawa <szymonwarszawa@gmail.com>
2021-02-07 13:53:45 +0000

    Add edge case that returns zero if given array is empty.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const Calculate = {
   sum(inputArray) {
     if (Array.isArray(inputArray)) {
+      if (inputArray.length === 0) {
+        return 0;
+      }
       return inputArray.reduce((a, b) => a + b);
     }
     return null;
--- test/index_test.js
+++ test/index_test.js
@@ -15,9 +15,21 @@ describe("Calculate", () => {
 
     it("returns the sum of a four-item list", () => {
       //setup
-      const expected = 10;
+      const expected = 22;
       //exercise
-      const inputArray = [1, 2, 3, 4];
+      const inputArray = [4, 5, 6, 7];
+
+      const actual = Calculate.sum(inputArray);
+      //verify
+      assert.equal(actual, expected);
+    });
+
+    it("returns zero for an empty array", () => {
+      //setup
+      const expected = 0;
+      //exercise
+      const inputArray = [];
+
       const actual = Calculate.sum(inputArray);
       //verify
       assert.equal(actual, expected);
